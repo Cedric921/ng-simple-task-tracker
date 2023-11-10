a4adf6c18b2ccf47863c27b5362e79eba8ee2386
cedric karungu <ckarungu921@gmail.com>
2023-11-10 22:33:01 +0200

    feat: use service and observable for data

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { TASKS } from 'src/app/mock.tasks';
+import { TaskService } from 'src/app/services/task.service';
 import { Task } from 'src/app/types/Task';
 
 @Component({
@@ -8,6 +8,11 @@ import { Task } from 'src/app/types/Task';
   styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
-  ngOnInit(): void {}
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
 }
