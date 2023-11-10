985228e57caca8b5170c58102f974ac2e905ca08
cedric karungu <ckarungu921@gmail.com>
2023-11-10 22:12:25 +0200

    feat: task fakes data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, ButtonComponent, TasksComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/mock.tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './types/Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Docotr Appointement',
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting vb',
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Shopping',
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  },
+];
